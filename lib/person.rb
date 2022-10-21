a70e20df7b57269f96cf4848edd586706ff336d7
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-10-21 15:49:24 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,87 @@
+
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def name
+        @name
+    end
+    def happiness=(happiness)
+        if happiness > 10
+            return @happiness = 10
+        elsif happiness < 0
+            return @happiness = 0
+        else
+            return @happiness = happiness
+        end
+    end
+    def happiness
+        @happiness
+    end
+    def hygiene=(hygiene)
+        if hygiene > 10
+            return @hygiene = 10
+        elsif hygiene < 0
+            return @hygiene = 0
+        else
+            return @hygiene = hygiene
+        end
+    end
+    def hygiene
+        @hygiene
+    end
+    def happy?
+        if happiness > 7
+            true
+        else
+            false
+        end
+    end
+    def clean?
+        if hygiene > 7
+            true
+        else
+            false
+        end
+    end
+    def get_paid(amount)
+        @bank_account += amount
+       "all about the benjamins"
+    end
+    def take_bath
+        self.hygiene = @hygiene += 4
+       "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    def work_out
+        self.happiness = @happiness += 2
+        self.hygiene = @hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+    def call_friend friend
+        self.happiness = @happiness += 3
+       friend.happiness = friend.happiness + 3 
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+    def start_conversation(friend_to_start, topic)
+           if topic == "politics"
+                self.happiness = @happiness -= 2
+                friend_to_start.happiness = friend_to_start.happiness - 2
+                return "blah blah partisan blah lobbyist"
+            elsif topic == "weather"
+                self.happiness = @happiness += 1
+                friend_to_start.happiness = friend_to_start.happiness + 1
+                return "blah blah sun blah rain"
+            else
+                return "blah blah blah blah blah"
+            end
+        
+    end
+end
+# p1 = Person.new("Christabel", 25)
